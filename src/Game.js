e0d0e7a485d355ee6c1efdd2c02eeac28f3db5cc
ilyasaitbel <ilyasilya084@gmail.com>
2025-11-10 09:22:04 +0100

    j'ai termini page de jeux

---
+++ src/Game.js
@@ -0,0 +1,57 @@
+let score = 0;
+let Tentatives = 5;
+
+const inputfound = document.getElementById("found");
+const scoreDisplay = document.getElementById("score");
+const Ten = document.getElementById("Tentatives");
+const buttons = document.querySelectorAll(".btn");
+
+const words = ["html", "ibangris", "ilyas"];
+let currentWord = "";
+let foundWord = [];
+
+
+function channge() {
+    currentWord = words[Math.floor(Math.random() * words.length)];
+    foundWord = Array(currentWord.length).fill("_");
+    inputfound.value = foundWord.join(" ");
+    buttons.forEach(btn => btn.disabled = false);
+    Ten.innerHTML = Tentatives;
+}
+
+function checkLetter(L, b) {
+    b.disabled = true;
+    let correct = false;
+
+    for (let i = 0; i < currentWord.length; i++) {
+        if (currentWord[i] === L) {
+            foundWord[i] = L;
+            correct = true;
+        }
+    }
+
+    inputfound.value = foundWord.join(" ");
+
+    if (correct) {
+        score += 10;
+        scoreDisplay.innerHTML = score;
+    } else {
+        Ten.innerHTML = --Tentatives;
+        if (Tentatives === 0) {
+            scoreDisplay.innerHTML = `<h2>Game Over</h2>`;
+            buttons.forEach(btn => btn.disabled = true);    
+        }
+    }
+    if (foundWord.join("") === currentWord) {
+        Ten.innerHTML = `<h2>Bravoo! Keep going</h2>`;
+        setTimeout(channge, 2000);
+    }
+}
+
+buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        checkLetter(btn.textContent, btn);
+    });
+});
+
+channge();
