b6760457a449514210347b36af23dc763e8d6eee
Amin332123 <mohamedaminerahmani6@gmail.com>
2025-11-11 09:52:39 +0100

    Commit for saving file before pullin

--- src/Index.js
+++ src/Index.js
@@ -1,8 +1,26 @@
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
 
-const btn = document.getElementById('menu-btn');
-const menu = document.getElementById('menu');
+// btn.addEventListener("click", () => {
+//   menu.classList.toggle("hidden");
+// });
 
-btn.addEventListener('click', () => {
-    menu.classList.toggle('hidden');
-});
+const FormationAdminCard = `<section class="w-[80%] ml-[10%] mt-[5%] h-[250px] grid justify-center rounded-lg border-2 border-[#00A6C0] md:w-[45%] md:ml-[2%] lg:w-[90%]">
+  <h3 class="text-2xl font-semibold text-[#00A6C0] mb-2 text-center mt-[5%]">{theme}</h3>
+  <p><strong>Trainer : </strong> <span class="trainerr">{Trainer}</span></p>
+  <p><strong>Duration : </strong><span class="Durationn">{Duration} </span> Mois</p>
+  <p><strong>Capacity : </strong> <span class="capacity">{Capacity}</span> poeple</p>
+  <p><strong>participants : </strong> <span class="participants">{participants}</span> participants</p>
 
+  <div class="flex justify-between">
+    <button class="rounded p-[5px] bg-green-600 text-white">Modify</button>
+    <button class="rounded p-[5px] bg-red-700 text-white">Delete</button>
+  </div>
+  <p class="mt-2"></p>
+</section>`;
+
+ fetch('../data/formation.json')
+    .then(Res => Res.json())
+    .then(data => {
+      
+    } )
