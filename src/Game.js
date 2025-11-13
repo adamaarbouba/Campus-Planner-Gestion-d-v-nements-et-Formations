eea2921a74bd6ed44df6b6964fc40f22815abead
ilyasaitbel <ilyasilya084@gmail.com>
2025-11-13 18:24:19 +0100

    j'ai terminé page de Game

--- src/Game.js
+++ src/Game.js
@@ -1,28 +1,47 @@
 let score = 0;
 let Tentatives;
 
-const inputfound = document.getElementById("found");
+const hint = document.getElementById("hint")
+
+const input = document.getElementById("input");
 const scoreDisplay = document.getElementById("score");
-const Ten = document.getElementById("Tentatives");
-const bob = document.getElementById("b")
+const Tentative = document.getElementById("Tentatives");
+const button = document.getElementById("Recomencer")
 const buttons = document.querySelectorAll(".btn");
 
-const words = ["html", "ibangris", "ilyas"];
-let currentWord = "";
 let foundWord = [];
-
-
+if(localStorage.length == 0)
+{
+async function nice() {
+    try {
+        const response = await fetch('https://mocki.io/v1/f64d2ff5-72de-47ea-a59d-c8e8bfae08ef')
+        const data = await response.json()
+        localStorage.setItem("data_response", JSON.stringify(data) )
+        console.log(data)
+        return data
+    } catch (error) {
+        console.error("error fetching data")
+    }
+}
+nice()
+}
 function channge() {
+    let data = JSON.parse(localStorage.getItem('data_response'));
     console.log("dfghjkg");
-    currentWord = words[Math.floor(Math.random() * words.length)];
+    index = Math.floor(Math.random() * data.length )
+    currentWord = data[index].word;
+    hint.textContent = data[index].hint
     foundWord = Array(currentWord.length).fill("_");
-    inputfound.value = foundWord.join(" ");
+    input.value = foundWord.join(" ");
     buttons.forEach(btn => btn.disabled = false);
-    score = 0;
+     
     Tentatives = 5;
-    bob.innerHTML = " "
-    scoreDisplay.innerHTML = score;
-    Ten.innerHTML = Tentatives;
+    button.innerHTML = " "
+    if((localStorage.getItem('sc')).length != 0){
+        score = JSON.parse(localStorage.getItem('sc'))
+        scoreDisplay.innerHTML = score;
+    }
+    Tentative.innerHTML = Tentatives;
 }
 
 function checkLetter(btn) {
@@ -36,29 +55,29 @@ function checkLetter(btn) {
         }
     }
 
-    inputfound.value = foundWord.join(" ");
-
+    input.value = foundWord.join(" ");
     if (correct) {
         score += 10;
-        scoreDisplay.innerHTML = score;
+        scoreDisplay.innerHTML = score
     } else {
-        Ten.innerHTML = --Tentatives;
+        Tentative.innerHTML = --Tentatives;
         if (Tentatives === 0) {
             scoreDisplay.innerHTML = `<h2>Game Over</h2>`;
-            bob.innerHTML = `<button class="btn border-2 border-[#00a6c0] p-2" onClick="channge()">Recomencer</button>`
-            buttons.forEach(btn => btn.disabled = true);    
+            button.innerHTML = `<button class="btn border-2 border-[#00a6c0] p-2" onClick="channge()">Recomencer</button>`
+            buttons.forEach(btn => btn.disabled = true);
+            score = 0
+        localStorage.setItem('sc',score)
         }
     }
     if (foundWord.join("") === currentWord) {
-        Ten.innerHTML = `<h2>Bravoo! Keep going</h2>`;
+        Tentative.innerHTML = `<h2>Bravoo! Keep going</h2>`;
+        localStorage.setItem("sc", JSON.stringify(score))
         setTimeout(channge, 2000);
     }
 }
-
 buttons.forEach(btn => {
     btn.addEventListener("click", () => {
         checkLetter(btn);
     });
 });
-
-channge();
+channge()
